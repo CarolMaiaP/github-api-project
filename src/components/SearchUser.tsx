253d08c3913f831f396ddb0d101a71c3242c7b85
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2023-03-14 16:54:25 -0300

    feat: repos list added

--- src/components/SearchUser.tsx
+++ src/components/SearchUser.tsx
@@ -7,9 +7,15 @@ interface userProps{
   avatar_url: string, 
 }
 
+interface reposProps{
+  name: string,
+  html_url: string,
+}
+
 export function SearchUser(){
   const [user, setUser] = useState('')
   const [findUser, setFindUser] = useState<userProps>()
+  const [repos, setRepos] = useState<reposProps[]>()
 
   async function handleSearchUser(){
     const response = await api.get(`/users/${user}`)
@@ -17,22 +23,51 @@ export function SearchUser(){
     setFindUser(response.data)
   }
 
+  async function handleUserRepos(){
+    const res = await api.get(`https://api.github.com/users/${user}/repos`)
+    console.log(res.data)
+    setRepos(res.data)
+  }
+
   return(
     <>
       <div className="search">
-        <input type="text" onChange={(event) => setUser(event.target.value)} />
+        <input type="text" placeholder="Type a github username" onChange={(event) => setUser(event.target.value)} />
         <button onClick={handleSearchUser}>Search</button>
       </div>
       <div className="container">
           {findUser ?
-            <div className="user">
-              <h4>{findUser.name}</h4>
-              <img src={findUser.avatar_url} alt={findUser.name} />
-              <p><strong>{findUser.login}</strong></p>
+            <div className="users">
+              <div className="user">
+                <img src={findUser.avatar_url} alt={findUser.name} />
+                <div className="user-infos">
+                  <h4>{findUser.name}</h4>
+                  <p><strong>{findUser.login}</strong></p>
+                </div>
+              </div>
+              <div className="repo-button">
+                <button onClick={handleUserRepos}>Load Repos</button>
+              </div>
             </div>
           :
             <>
             </>
+          }
+
+          {repos ?
+            <div className="repos">
+              {repos.map((repo) => {
+                return(
+                  <div className="repo">
+                    <a href={repo.html_url} target="_blank">
+                     <h4>{repo.name}</h4>
+                    </a>
+                  </div>
+                )
+              })}
+            </div>
+          :
+            ""
           }
       </div>
     </>
